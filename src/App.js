07183c2b92e7715dfdb63d3719a64f81a59ebadc
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-03-02 20:21:57 -0300

    Add myaccount form and edition

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MyAccount from './components/MyAccount';
 
 function App() {
   return (
     <Router>
-      <Route>
-        <NavBar />
-      </Route>
-      <Switch>
+      <div className='d-flex justify-content-start'>
         <Route>
-          <Home path="/" />
+          <NavBar />
         </Route>
-      </Switch>
+        <Switch>
+          <div className='mx-auto my-5'>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/myaccount">
+              <MyAccount />
+            </Route>
+          </div>
+        </Switch>
+      </div>
     </Router>
   )
 }
--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,7 +1,64 @@
 import React from 'react'
+import { Button, Card, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import usuario from "../assets/usuario1.png"
 
 export default function MyAccount() {
     return (
-        <div>Account</div>
+        <div className='myaccount-container'>
+            <div>
+                <Card className='card-container'>
+                    <Card.Img variant="center" src={usuario} className="card-image p-2" />
+                    <Card.Body className='d-flex flex-column'>
+                        <Link to="#" className='fw-bold'>Subir una nueva foto</Link>
+                        <Link to="#" >Quitar foto</Link>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Card className='card-form-container'>
+                    <Form>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicName">
+                            <Form.Label className='form-label'>Nombre</Form.Label>
+                            <Form.Control className='form-control' type="text" />
+                        </Form.Group>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicLastName">
+                            <Form.Label className='form-label'>Apellido</Form.Label>
+                            <Form.Control className='form-control' type="text" />
+                        </Form.Group>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicEmail">
+                            <Form.Label className='form-label-double'>Correo electrónico</Form.Label>
+                            <Form.Control className='form-control' type="email" placeholder='rufian.contreri@company.com' />
+                        </Form.Group>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicOrg">
+                            <Form.Label className='form-label'>Organización</Form.Label>
+                            <Form.Control className='form-control' type="text" placeholder='My Company' />
+                        </Form.Group>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicBirthDate">
+                            <Form.Label className='form-label-double'>Fecha de nacimiento</Form.Label>
+                            <Form.Control className='form-control' type="date" />
+                        </Form.Group>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicWeight">
+                            <Form.Label className='form-label'>Peso</Form.Label>
+                            <Form.Control className='form-control' type="text" placeholder='kg' />
+                        </Form.Group>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicHeight">
+                            <Form.Label className='form-label'>Talla</Form.Label>
+                            <Form.Control className='form-control' type="text" placeholder='cm' />
+                        </Form.Group>
+                        <Form.Group className="mb-1 d-flex" controlId="formBasicPassword">
+                            <Form.Label className='form-label'>Contraseña</Form.Label>
+                            <Form.Control className='form-control' type="password" placeholder='**********' />
+                        </Form.Group>
+                        <div className='d-flex flex-column align-items-end'>
+                            <Link to="#">Cambiar contraseña</Link>
+                            <Button className='form-button' variant="primary" type="submit">
+                                Guardar
+                            </Button>
+                        </div>
+                    </Form>
+                </Card>
+            </div>
+        </div>
     )
 }
