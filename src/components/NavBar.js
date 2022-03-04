698a3638b1b590ead18d90657fa17d56eeebfa06
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-03-03 23:39:52 -0300

    Actions to sidebar and aictive logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,38 +3,40 @@ import { faHouse, faUser, faGear, faGrip, faCircleQuestion, faArrowRightFromBrac
 import React from 'react'
 import { Navbar, Nav, Container } from "react-bootstrap"
 import { Link } from 'react-router-dom'
+import aictiveLogo from "../assets/aictive-logo.png"
 
 export default function NavBar() {
+
+    const handleClick = () => {
+        document.getElementById("sidebar-hide").classList.toggle("sidebar-hide")
+    }
+
     return (
-        <Navbar bg="light" expand="lg" className='sidebar'>
+        <Navbar bg="light" expand="lg" className='sidebar' id="sidebar-hide">
+            <div className='logo-container'><Link to="#" onClick={handleClick}><img src={aictiveLogo} alt="aictive" className="aictive-logo" /></Link></div>
             <Container className='sidebar-container'>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className='flex-column icons-column'>
-                    <Nav className="me-auto flex-column text-start" style={{ width: "260px" }}>
-                        <div className='d-flex justify-content-between'>
-                            <Link href="#home">Inicio</Link>
-                            <FontAwesomeIcon icon={faHouse} className="mt-2" />
+                    <Nav className="me-auto justify-content-between text-start" style={{ width: "260px" }}>
+                        <div>
+                            <ul className='links-wrapper'>
+                                <li className='links-sidebar'><Link className='links-text' to="/">Inicio</Link></li>
+                                <li className='links-sidebar'><Link className='links-text' to="myaccount">Mi cuenta</Link></li>
+                                <li className='links-sidebar'><Link className='links-text' href="#link">Ajustes</Link></li>
+                                <li className='links-sidebar'><Link className='links-text' href="#link">Aplicaciones</Link></li>
+                                <li className='links-sidebar'><Link className='links-text' href="#link">Soporte</Link></li>
+                                <li className='link-sidebar-closeSession'><Link className='links-text' href="#link">Cerrar Sesión</Link></li>
+                            </ul>
                         </div>
-                        <div className='d-flex justify-content-between'>
-                            <Link to="myaccount">Mi cuenta</Link>
-                            <FontAwesomeIcon icon={faUser} className="mt-2" />
-                        </div>
-                        <div className='d-flex justify-content-between'>
-                            <Link href="#link">Ajustes</Link>
-                            <FontAwesomeIcon icon={faGear} className="mt-2" />
-                        </div>
-                        <div className='d-flex justify-content-between'>
-                            <Link href="#link">Aplicaciones</Link>
-                            <FontAwesomeIcon icon={faGrip} className="mt-2" />
-                        </div>
-                        <div className='d-flex justify-content-between'>
-                            <Link href="#link">Soporte</Link>
-                            <FontAwesomeIcon icon={faCircleQuestion} className="mt-2" />
-                        </div>
-                        <div className='d-flex justify-content-between mt-5'>
-                            <Link href="#link" className='text-start'>Cerrar Sesión</Link>
-                            <FontAwesomeIcon icon={faArrowRightFromBracket} className="mt-2" />
+                        <div>
+                            <ul className='icons-wrapper'>
+                                <li className='icons-sidebar-li'><Link className='links' to="/"><FontAwesomeIcon icon={faHouse} className="icons-sidebar" /></Link></li>
+                                <li className='icons-sidebar-li'><Link className='links' to="myaccount"><FontAwesomeIcon icon={faUser} className="icons-sidebar" /></Link></li>
+                                <li className='icons-sidebar-li'><Link className='links' href="#link"><FontAwesomeIcon icon={faGear} className="icons-sidebar" /></Link></li>
+                                <li className='icons-sidebar-li'><Link className='links' href="#link"><FontAwesomeIcon icon={faGrip} className="icons-sidebar" /></Link></li>
+                                <li className='icons-sidebar-li'><Link className='links' href="#link"><FontAwesomeIcon icon={faCircleQuestion} className="icons-sidebar" /></Link></li>
+                                <li className='icons-sidebar-li'><Link className='links' href="#link"><FontAwesomeIcon icon={faArrowRightFromBracket} className="icon-sidebar-closeSession" /></Link></li>
+                            </ul>
                         </div>
                     </Nav>
                 </Navbar.Collapse>
